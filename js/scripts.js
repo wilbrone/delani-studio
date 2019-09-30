4d8b90612ef4b1f7d9a6b1471e01f2715585e6e4
wilbrone <wilbroneokoth@gmail.com>
2019-09-30 09:13:53 +0300

    add thank you note

--- js/scripts.js
+++ js/scripts.js
@@ -99,7 +99,7 @@ $('#contactus-form').bind('submit', function(event) {
         console.log(errorThrown);
       }
   });  //End of ajax
-
+  alert(name + " we have received your message. Thank you fro reaching out to us.")
 });  //End of click bind
 
 
